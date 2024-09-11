0cc805ec06aac496926a7e0bc8cf04664e480ea2
Envisational <insight.da101@gmail.com>
2024-09-11 16:33:10 +0300

    TODO: move router to controller logic + public route

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import express from "express";
 import { Request, Response } from "express";
 import blogRoutes from "./routes/blogRoutes.js"
 import dotenv from 'dotenv';
-import authMiddleware from "./middlewares/authMiddleware.js"; 
 import authRoutes from './routes/authRoutes.js';
 import connectDB from "./config/db.js"; 
+import Blog from "./models/Blog.js";
 
 dotenv.config();
 
@@ -15,13 +15,20 @@ app.use(express.json());
 // Connect to the DB
 await connectDB();
 
+
+// Get all posts (public) TODO: Enable cors or limiter 
+app.get('/public', async (req: Request, res: Response) => {
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 }); // Sort by newest
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to get blogs. Details:', error });
+  }
+});
+
+
 // Use the auth routes for registration and login
 app.use('/auth', authRoutes); 
-
-// TEST API
-app.get('/', (req: Request, res: Response) => {
-    res.send("API is running!");
-});
 
 // Protected Admin route
 app.use('/admin/blogs', blogRoutes);
